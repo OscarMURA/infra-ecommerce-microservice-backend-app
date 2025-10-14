5226aa8973cc3eebd0c2da8f335f7695d79cb18f
Oscar Stiven Muñoz Ramirez <1083870123@u.icesi.edu.co>
2025-10-13 20:37:47 -0500

    fix: update Kubernetes version to 1.30.2-do.0 - current stable version for DOKS

--- terraform/environments/dev/doks/variables.tf
+++ terraform/environments/dev/doks/variables.tf
@@ -23,7 +23,7 @@ variable "region" {
 variable "kubernetes_version" {
   description = "Versión de Kubernetes"
   type        = string
-  default     = "1.28.2-do.0"
+  default     = "1.30.2-do.0"  # Versión actualizada y estable
 }
 
 variable "vpc_ip_range" {
