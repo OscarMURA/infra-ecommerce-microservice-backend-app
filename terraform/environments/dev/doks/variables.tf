fa76127ffb25f9b7eb8a485b385458103bfa9e5b
Oscar Stiven Muñoz Ramirez <1083870123@u.icesi.edu.co>
2025-10-13 20:34:38 -0500

    fix: change VPC IP range to 10.116.0.0/16 - safe range for DOKS

--- terraform/environments/dev/doks/variables.tf
+++ terraform/environments/dev/doks/variables.tf
@@ -29,7 +29,7 @@ variable "kubernetes_version" {
 variable "vpc_ip_range" {
   description = "Rango de IPs para la VPC"
   type        = string
-  default     = "10.244.0.0/16"  # Rango no reservado por DigitalOcean
+  default     = "10.116.0.0/16"  # Rango seguro y válido para DOKS
 }
 
 # Node Pool
